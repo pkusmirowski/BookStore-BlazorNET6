5d1708829934794fa256ff3409b876648941dacc
Paweł Kuśmirowski <56277371+pkkw777@users.noreply.github.com>
2022-05-12 19:45:56 +0200

    Completed Author API

--- BookStore.API/Data/BookStoreDbContext.cs
+++ BookStore.API/Data/BookStoreDbContext.cs
@@ -23,7 +23,6 @@ namespace BookStore.API.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=DESKTOP-VJR295T;Database=BookStoreDb;Trusted_Connection=True;MultipleActiveResultSets=True");
             }
         }
---
+++ BookStore.API/Static/Messages.cs
@@ -0,0 +1,7 @@
+﻿namespace BookStore.API.Static
+{
+    public static class Messages
+    {
+        public const string Error500 =  "There was an error completing your request. Please try again later.";
+    }
+}
